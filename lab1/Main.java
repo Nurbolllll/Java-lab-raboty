52e0855b125dd1cd44d5c115a0048639440658cf
Nurbolllll <ntalgatov2007nur@gmail.com>
2026-02-09 13:47:02 +0600

    lab1

---
+++ lab1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String fullName = "Нурбол Тал";
+        String group = "ИС24-22";
+        String specialty = "Информационные системы";
+
+        System.out.println("Информация о студенте:");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("Специальность: " + specialty);
+    }
+}
